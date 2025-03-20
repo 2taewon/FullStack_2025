35de03ad8e0fce423ff95b6cc939836fe5c5f332
kosa <kosa@DESKTOP-TAJ37DT>
2025-03-20 17:57:14 +0900

    일일과제 알고리즘

---
+++ day02_datatype/src/Homework/no1.java
@@ -0,0 +1,19 @@
+package Homework;
+
+//이태원
+
+import java.util.Scanner;
+
+class no1{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	
+	System.out.println(a + b);
+	System.out.println(a - b);
+	System.out.println(a * b);
+	System.out.println(a / b);
+	System.out.println(a % b);
+	}
+}
